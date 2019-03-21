4411366dc76e7d91b1ff3c3da5e7969de6446b56
Azern32 <chiro.emiya@gmail.com>
2019-03-21 11:43:28 +0800

    yeet

---
+++ assets/firebaseinit.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBp2C3PYzLR_4W275cIv4X06fg1ldOIaIo",
+    authDomain: "riset-konsumen.firebaseapp.com",
+    databaseURL: "https://riset-konsumen.firebaseio.com",
+    projectId: "riset-konsumen",
+    storageBucket: "riset-konsumen.appspot.com",
+    messagingSenderId: "750585038868"
+  };
+  firebase.initializeApp(config);
+</script>
